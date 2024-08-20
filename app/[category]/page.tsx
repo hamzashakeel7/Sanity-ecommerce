4d37607e05dfcf698c089a688d0356a3e24bbda9
hamza durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-08-20 20:43:40 +0500

    solving errors 3

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -4,38 +4,30 @@ import { simplifiedProduct } from "../interface";
 import Link from "next/link";
 import Image from "next/image";
 
-// Function to fetch the newest products, with an optional category filter
-async function getData(category?: string) {
-  const categoryFilter = category ? `&& category->name == "${category}"` : "";
-  const query = `*[_type == "product" ${categoryFilter}] | order(_createdAt desc)[0...4] {
-    _id,
-    "imageUrl": images[0].asset->url,
+async function getData(category: string) {
+  const query = `*[_type == "product" && category->name == "${category}"] {
+  _id,
+  "imageUrl": images[0].asset->url,
     price,
     name,
     "slug": slug.current,
     "categoryName": category->name
-  }`;
-
+}`;
   const data = await client.fetch(query);
   return data;
 }
 
-// Ensuring the page is dynamically rendered on each request
 export const dynamic = "force-dynamic";
 
-const Newest = async ({ category }: { category?: string }) => {
-  const data: simplifiedProduct[] = await getData(category);
-
+const CategoryPage = async ({ params }: { params: { category: string } }) => {
+  const data: simplifiedProduct[] = await getData(params.category);
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            {category ? `${category} Products` : "Our Latest Products"}
+            {params.category} Products
           </h2>
-          <Link href="/all" className="text-primary flex items-center gap-x-1">
-            View More{" "}
-          </Link>
         </div>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -48,7 +40,7 @@ const Newest = async ({ category }: { category?: string }) => {
                 <div className="aspect-square w-full rounded-md overflow-hidden bg-gray-200 group-hover:opacity-75 lg:h-80">
                   <Image
                     src={product.imageUrl}
-                    alt={product.name}
+                    alt=""
                     className="h-full w-full object-cover object-center lg:w-full lg:h-full"
                     width={300}
                     height={300}
@@ -74,4 +66,4 @@ const Newest = async ({ category }: { category?: string }) => {
   );
 };
 
-export default Newest;
+export default CategoryPage;
