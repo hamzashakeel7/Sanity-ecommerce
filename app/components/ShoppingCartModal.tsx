dbc47e04413e552e63d7272bac678e9597856df3
hamza durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-08-30 21:09:55 +0500

    trying to solve error

--- app/components/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal.tsx
@@ -86,7 +86,7 @@ const ShoppingCartModal: React.FC = () => {
                       <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                         <Image
                           alt={entry.name}
-                          src={entry.image}
+                          src={entry.image ?? ""}
                           width={100}
                           height={100}
                         />
